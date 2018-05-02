6092645898473326c2c99f7d59c8c65f655c864f
rogersonpi <iain.rogerson77@googlemail.com>
2018-05-02 13:44:04 +0100

    original_pizza_shop

---
+++ pizza_controller.rb
@@ -0,0 +1,32 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/pizza_order.rb')
+
+
+# READ orders
+get '/pizza_orders' do
+  @orders = PizzaOrder.all()
+  erb(:index)
+  # CREATE order
+end
+
+get '/pizza_orders/new' do
+  erb(:new)
+end
+post '/pizza_orders' do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+end
+
+#Show order
+get '/pizza_orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+
+# UPDATE existing orders
+
+# DELETE orders
